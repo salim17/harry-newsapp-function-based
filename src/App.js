ea98de0bfe7ab615a82e9b888708b85395f38ca9
Mohd Salim <mohd.salim@ibm.com>
2024-05-06 00:49:57 +0530

    news app function based

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+const App = (props) => {
+  const pageSize = 5;
+  const apiKey = process.env.REACT_APP_NEWS_API;
+
+  const [progress, setProgress] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <LoadingBar color="#f11946" height={3} progress={progress} />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <div>
+              <News
+                setProgress={setProgress}
+                apiKey={apiKey}
+                key="general"
+                pageSize={pageSize}
+                country="in"
+                category="general"
+              />
+            </div>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/business"
+          element={
+            <div>
+              <News
+                setProgress={setProgress}
+                apiKey={apiKey}
+                key="business"
+                pageSize={pageSize}
+                country="in"
+                category="business"
+              />
+            </div>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/entertainment"
+          element={
+            <div>
+              <News
+                setProgress={setProgress}
+                apiKey={apiKey}
+                key="entertainment"
+                pageSize={pageSize}
+                country="in"
+                category="entertainment"
+              />
+            </div>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/science"
+          element={
+            <div>
+              <News
+                setProgress={setProgress}
+                apiKey={apiKey}
+                pageSize={pageSize}
+                country="in"
+                category="science"
+              />
+            </div>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/health"
+          element={
+            <div>
+              <News
+                setProgress={setProgress}
+                apiKey={apiKey}
+                key="health"
+                pageSize={pageSize}
+                country="in"
+                category="health"
+              />
+            </div>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/sports"
+          element={
+            <div>
+              <News
+                setProgress={setProgress}
+                apiKey={apiKey}
+                key="sports"
+                pageSize={pageSize}
+                country="in"
+                category="sports"
+              />
+            </div>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/technology"
+          element={
+            <div>
+              <News
+                setProgress={setProgress}
+                apiKey={apiKey}
+                key="technology"
+                pageSize={pageSize}
+                country="in"
+                category="technology"
+              />
+            </div>
+          }
+        ></Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
